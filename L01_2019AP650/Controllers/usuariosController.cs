8203793ff717918c1027221afd8bd40594dd239c
Fabry99 <irving.abrego@catolica.edu.sv>
2023-03-04 11:11:59 -0600

    Agregar archivos de proyecto.

---
+++ L01_2019AP650/Controllers/usuariosController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using L01_2019AP650.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace L01_2019AP650.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usuariosController : ControllerBase
+    {
+
+        private readonly entidades _entidades;
+        
+        public usuariosController(entidades entidad)
+        {
+            _entidades = entidad;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get() { 
+            
+            List<usuarios> listadousuarios =(from e in _entidades.usuarios
+                                             select e).ToList();
+
+            if (listadousuarios.Count()==0)
+            {
+                return NotFound();
+                
+            }
+            return Ok(listadousuarios);
+
+        }
+    }
+}
---
+++ L01_2019AP650/Models/entidades.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.Identity.Client;
+using System.Threading.Tasks.Dataflow;
+
+
+namespace L01_2019AP650.Models
+{
+    public class entidades : DbContext
+    {
+        public entidades(DbContextOptions<entidades> options) : base (options) 
+        {
+            
+
+        }
+        public DbSet<usuarios> usuarios { get; set; }   
+    }
+}
